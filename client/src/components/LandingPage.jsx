ffdac99ea6376e559d025bbe1e4214030212a72e
XDrimer <tutemaposo2016@gmail.com>
2022-06-10 02:47:21 -0300

    final

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -5,6 +5,7 @@ import styles from "./LandingPage.module.css";
 function LandingPage() {
   return (
     <div className={styles.landingContainer}>
+      <h1 className={styles.responsive}>Sorry not responsive at the moment , open the app in a computer</h1>
       <h1 className={styles.title}>Pokemon APP</h1>
       <Link to="/home" ><button className={styles.link}>Click here to enter</button></Link>
     </div>
